41b0d683ee4eaaa1b60eec075e3fd37bb3773c5b
adityamakkar000 <aditymakkar000@gmail.com>
2025-10-25 22:18:58 -0400

    v1 of sharding

---
+++ src/stax/logger/logger.py
@@ -0,0 +1,16 @@
+
+
+class WandBLogger:
+
+    def __init__(self): 
+        self.train_loss = 0.0
+
+    def log(self, data: dict): 
+        pass 
+
+
+
+
+
+
+
---
+++ src/stax/sharding/main.py
@@ -0,0 +1,54 @@
+import jax 
+import numpy as np
+from jaxtyping import PyTree
+from loguru import logger
+
+from jax.sharding import (
+    NamedSharding, 
+    PartitionSpec as P, 
+    Mesh
+)
+
+"""
+TODO: 
+add multihost sharding 
+FSDP sharding
+integrate with training loop to add Zero 1,2,3 
+"""
+
+def setup_dp(devices : np.ndarray | None = None):
+
+    if devices is None:
+        devices = np.array(jax.devices())
+
+    # sometimes jax cannot create optimal mesh layout
+    # in that case manually make it
+    try:
+        mesh = jax.make_mesh((len(devices), ) ('dp',), devices=devices)
+    except:
+        logger.warning("Failed to create mesh with make_mesh, falling back to sharding.Mesh")
+        mesh = Mesh(devices, ('dp',))
+
+    logger.info(f"setup DP mesh with {mesh}")
+    return mesh
+
+def get_dp_sharding(mesh : Mesh, params: PyTree, opt_state: PyTree, data_axis : int = 0) -> dict[str, PyTree |callable]:
+
+    """inspired by https://github.com/kvfrans/jaxtransformer"""
+    assert len(mesh.axis_names) == 1, f"dp mesh should only have one mesh"
+
+    replicate_sharding = NamedSharding(mesh, P())
+    data_sharding = NamedSharding(mesh, P(*(None for _ in range(data_axis - 1)), mesh.axis_names[0])) 
+
+    param_sharding = replicate_sharding
+    opt_state_sharding = replicate_sharding 
+
+    def shard_data(*batch):
+        batch = jax.tree.map(lambda x: jax.device_put(x, data_sharding))
+        return batch if len(batch) > 1 else batch[0]
+
+    return {"param_sharding": param_sharding, "opt_state_sharding": opt_state_sharding, "shard_data": shard_data}
+
+if __name__ == '__main__':
+    mesh = setup_dp()
+    breakpoint()
