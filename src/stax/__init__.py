12733c04832e9ae2db4ebc4873577e99435cd5f1
adityamakkar000 <aditymakkar000@gmail.com>
2025-10-28 22:19:32 -0400

    make sharding local

--- src/stax/__init__.py
+++ src/stax/__init__.py
@@ -1,5 +1,5 @@
 from stax.checkpointer import *
 from stax.fn import *
-from stax.sharding import *
+# from stax.sharding import *
 
-__all__ = [Checkpointer, get_steps_fn, setup_dp, get_dp_sharding]
+__all__ = [Checkpointer, get_steps_fn]
