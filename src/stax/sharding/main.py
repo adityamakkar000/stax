69a399ab1583c3a04e3dd017258a703de6c67208
adityamakkar000 <aditymakkar000@gmail.com>
2025-10-28 22:51:45 -0400

    update pipeline

--- src/stax/sharding/main.py
+++ src/stax/sharding/main.py
@@ -53,7 +53,7 @@ def get_dp_sharding(mesh : Mesh, data_axis : int = 0) -> dict[str, Union[PyTree,
     def shard_data(batch):
         # special function to handle keys in batch
         def put_batch_fn(x):
-            is_key = lambda x:  x.ndim == 2 and x.shape[0] == 1 and x.shape[1] == 2:
+            is_key = lambda x:  (x.ndim == 2 and x.shape[0] == 1 and x.shape[1] == 2)
             if is_key(x): 
                 x = jax.array(jax.random.split(x, jax.device_count()))
             return jax.device_put(x, data_sharding)
