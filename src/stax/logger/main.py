bcb7f8b66bbf06cd4370160a87aaca7564bf91a9
adityamakkar000 <aditymakkar000@gmail.com>
2025-12-13 17:19:33 -0500

    update logger

--- src/stax/logger/main.py
+++ src/stax/logger/main.py
@@ -7,39 +7,48 @@ import itertools as it
 
 from loguru import logger
 import abc
-
-
-def convert_to_scalar(x):
-    return x.item() if isinstance(x, Array) else x
+from stax.utils import convert_to_scalar
 
 
 class BaseLogger(abc.ABC):
     def __init__(self, metrics_to_print: list[str] = ["loss"]):
-        self.prev_metric = None
+        self.prev_metrics = None
         self.metric_to_print = metrics_to_print
+        if jax.process_index() == 0:
+            self.setup_logger()
 
     def __call__(self, step: int, data: dict[str, any]):
-        cur_metrics = {"step": step, "data": data}
-        self.prev_metrics, log_metrics = cur_metrics, self.prev_metric
-        if log_metrics is None:
-            return
-        self.async_log(**log_metrics)
-        self._log(**log_metrics)
+        if jax.process_index() == 0:
+            cur_metrics = {"step": step, "data": data}
+            self.prev_metrics, log_metrics = cur_metrics, self.prev_metrics
+            if log_metrics is None:
+                return
 
-    def _log(self, step: int, metrics: dict[str, any]):
+            self.async_log(**log_metrics)
+            self._log(**log_metrics)
+
+    def _log(self, step: int, data: dict[str, any]):
         log_str = it.starmap(
             lambda k, v: f"{k}: {convert_to_scalar(v):.4f}",
-            filter(lambda kv: kv[0] in self.metric_to_print, metrics.items()),
+            filter(lambda kv: kv[0] in self.metric_to_print, data.items()),
         )
         fmt_str = " | ".join((f"Step : {step}\t\t", *log_str))
         logger.info(fmt_str)
+
+    def finish(self):
+        if jax.process_index() == 0:
+            self._finish()
+
+    @abc.abstractmethod
+    def setup_logger(self):
+        raise NotImplementedError("base class ")
 
     @abc.abstractmethod
     def async_log(self, step: int, data):
         raise NotImplementedError("base class ")
 
     @abc.abstractmethod
-    def finish(self):
+    def _finish(self) -> None:
         raise NotImplementedError("base class ")
 
     @property
@@ -52,10 +61,13 @@ class TextLogger(BaseLogger):
     def __init__(*args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def setup_logger(self):
+        pass
+
     def async_log(self, step: int, data: dict[str, Any]):
         pass
 
-    def finish(self) -> None:
+    def _finish(self) -> None:
         pass
 
     @property
@@ -73,7 +85,6 @@ class WandBLogger(BaseLogger):
         *args,
         **kwargs,
     ):
-        super().__init__(*args, **kwargs)
         assert (config is not None) or (run_id is not None), (
             "Either config or run_id must be provided"
         )
@@ -84,17 +95,20 @@ class WandBLogger(BaseLogger):
         else:
             init_args["config"] = config
 
-        self._run = wandb.init(
-            **init_args,
-        )
+        self.init_args = init_args
+        super().__init__(*args, **kwargs)
 
+    def setup_logger(self):
+        self._run = wandb.init(
+            **self.init_args,
+        )
         logger.info(f"Initialized WandB Logger with run id {self.id}")
 
     def async_log(self, step: int, data: dict[str, Any]):
         data = jax.tree.map(convert_to_scalar, data)
         self._run.log(data, step=step)
 
-    def finish(self) -> None:
+    def _finish(self) -> None:
         self._run.finish()
 
     @property
