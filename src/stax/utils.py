6bb1b03a0c3c2ee3b416c58b1eb1d54d8313b960
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-08 15:08:37 -0500

    fix formating

--- src/stax/utils.py
+++ src/stax/utils.py
@@ -206,8 +206,8 @@ def init_distrbuted_jax():
     if jax.process_index() == 0:
         process_count = jax.process_count()
         local_devices = len(jax.local_devices())
-        logger.info(f"JAX distributed initialized with {process_count} processes with {local_devices} per host.")
         all_devices = jax.devices()
-        print(f"Total devices available: {len(all_devices)}")
+        logger.info(f"JAX distributed initialized with {process_count} processes with {local_devices} per host.")
+        logger.info("Devices:")
         for dev in all_devices:
-            print(f"Device ID: {dev.id}, Platform: {dev.platform}, Kind: {dev.device_kind}")
+            logger.info(f"\tDevice ID: {dev.id}, Platform: {dev.platform}, Kind: {dev.device_kind}")
