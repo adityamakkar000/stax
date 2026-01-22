0c6c779d618109550e5e45472de9e0efcdda0b8b
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-21 20:31:21 -0500

    fix mesh logging

--- src/stax/sharding/main.py
+++ src/stax/sharding/main.py
@@ -70,7 +70,7 @@ def setup_mesh(devices: np.ndarray | None = None):
         # if jax cannot create optimal mesh layout, make a manual mesh
         logger.warning("Failed to create mesh with make_mesh, falling back to `jax.sharding.Mesh`")
         mesh = Mesh(devices, axis_names, axis_type)
-    logger.info(f"setup DP mesh with {mesh}")
+    logger.info(f"setup mesh : {mesh}")
     return mesh
 
 
