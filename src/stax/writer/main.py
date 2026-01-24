28a5c8294d79fae3999877602e8b0e9c8ac126c9
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-24 18:13:32 -0500

    try walrus

--- src/stax/writer/main.py
+++ src/stax/writer/main.py
@@ -45,10 +45,7 @@ class BaseMetricWriter(abc.ABC):
             if self._run is None:
                 logger.warning("no writer is set")
 
-        current_id = self.id
-        if current_id is None:
-            current_id = "n/a"
-        logger.info("Setup metric writer with id: %s", current_id)
+        logger.info(f"Setup writer with id: {_id if (_id := self.id) is not None else 'n/a'}")
         sync_global_devices("writer_setup")
 
     def __call__(self, step: int, data: PyTree):
