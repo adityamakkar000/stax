080e26cebb5b008814224d5fc907987973f4c273
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-29 15:09:48 -0500

    let more exports happen

--- src/stax/fn/__init__.py
+++ src/stax/fn/__init__.py
@@ -1,7 +1,10 @@
 from .main import get_steps_fn
-from .utils import StepFn
+from .utils import SingleStepFn, StepFn, TrainFn, ValFn
 
 __all__ = [
     "get_steps_fn",
     "StepFn",
+    "SingleStepFn",
+    "TrainFn",
+    "ValFn",
 ]
