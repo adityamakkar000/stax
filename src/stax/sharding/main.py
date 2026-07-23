c0f8f264d50672cdb08abe59bfd7395431b71746
adityamakkar000 <aditymakkar000@gmail.com>
2026-07-23 07:35:36 -0400

    update

--- src/stax/sharding/main.py
+++ src/stax/sharding/main.py
@@ -129,7 +129,7 @@ def get_sharding(mesh: Mesh, config: ShardingConfig) -> Shardings:
             else:
                 data_tuple[config.data_shard_dim] = (AXIS_NAMES_ENUM.DP.value, AXIS_NAMES_ENUM.FSDP.value)
                 data_tuple[config.cp_shard_dim] = AXIS_NAMES_ENUM.CP.value
-            data_sharding = NamedSharding(mesh, P(*(data_tuple)))
+            data_sharding = NamedSharding(mesh, P(*data_tuple,))
 
             num_hosts = mesh.devices.size // jax.local_device_count()
             x_shape = (
