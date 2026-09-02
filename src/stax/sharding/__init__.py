d663dab068a77bce3f92918200590f98723d2f22
divyamakkar0 <divyamakkar000@gmail.com>
2026-09-02 18:30:32 -0400

    added meshConfig to init

--- src/stax/sharding/__init__.py
+++ src/stax/sharding/__init__.py
@@ -1,13 +1,9 @@
-from stax.sharding.main import (
-    ShardingConfig,
-    Shardings,
-    get_sharding,
-    setup_mesh,
-)
+from stax.sharding.main import MeshConfig, ShardingConfig, Shardings, get_sharding, setup_mesh
 
 __all__ = [
     "setup_mesh",
     "get_sharding",
     "ShardingConfig",
     "Shardings",
+    "MeshConfig"
 ]
